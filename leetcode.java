24a2d2f7eacf21b26e58287bbba9cbf952aeb254
abhishekjha0910 <99851548+abhishekjha0910@users.noreply.github.com>
2022-10-29 12:16:13 +0530

    2)Maximum Product subarray

--- leetcode.java
+++ leetcode.java
@@ -1,19 +1,21 @@
 import java.util.*;
 public class leetcode {
-    public static void max_subarray(int number[]){
-        int max = Integer.MIN_VALUE;
-        int curr = 0;
-        for(int i  = 0 ; i<number.length ; i++){
-            curr += number[i];
-            max = Math.max(max,curr);
-            if(curr < 0){
-                curr = 0;
+    public static void max_product_subarray(int number[]){
+        int max = number[0] ;
+        int min = number[0];
+        int ans = number[0];
+        for(int i = 1 ; i<number.length ; i++){
+            int temp = max;
+            max = Math.max(Math.max(max*number[i],min*number[i]) ,number[i]);
+            min = Math.min(Math.min(temp*number[i] ,min*number[i]), number[i]);
+            if(max > ans){
+                ans = max;
             }
         }
-        System.out.println(max);
+        System.out.println(ans);
     }
     public static void main(String args[]){
         int number[] = {-2,0,-1};
-        max_subarray(number);
-    } 
+        max_product_subarray(number);
+    }
 }
